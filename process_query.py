b4ca903b9e0a937616e5db4d1a443ab790c0d9d3
Yoken0 <kenzomatsuo@gmail.com>
2024-01-29 18:59:41 -0500

    added function that only returns recipes that can be made with current ingredients

--- process_query.py
+++ process_query.py
@@ -3,16 +3,26 @@ import json
 with open("recipes.json", "r") as file:
     file = json.load(file)
 #TODO user input is set for now, will work on getting from website
-userInput  = ["butter"]
+userInput  = ["flour", "butter", "sugar"]
 
 # recipe list that contains the ingredients given
 recipesList = []
 
+# list with only recipes that can be made
+
+possibleRecipes = []
+
 def recipeSearcher(userInput):
     for i in file:
         test = i["Ingredients"]
-        if all(element in test for element in userInput):
+        if any(element in test for element in userInput):
             recipesList.append(i["Name"])
+# only includes recipes that can be made with current ingredients
+def possibleRecipeGen(userInput):
+    for i in file:
+        test = i["Ingredients"]
+        if all(element in test for element in userInput):
+            possibleRecipes.append(i["Name"])
 
-recipeSearcher(userInput)
-print(recipesList)
+possibleRecipeGen(userInput)
+print(possibleRecipes)
