c935727178869b647c4c1bd2f31da26dd30d9b09
ericanderson85 <ericandersonnn@icloud.com>
2024-01-29 13:08:29 -0500

    Force ingredients lowercase, name title and unique

--- add_recipes.py
+++ add_recipes.py
@@ -1,25 +1,39 @@
 import json
 
-# Create recipes list
-recipes = []
 
-# Open recipes.json and read it to recipes list
-with open("recipes.json", "r") as file:
-    recipes = json.load(file)
+def main():
+    # Create recipes list
+    recipes = []
 
-# Create new recipe to add
-name = "..."
-ingredients = ["Milk", "Butter"]
-link = "https://google.com/"
+    # Open recipes.json and read it to recipes list
+    with open("recipes.json", "r") as file:
+        recipes = json.load(file)
 
-# Create dictionary for new recipe
-new_recipe = {"Name": name,
-              "Ingredients": ingredients,
-              "Link": link}
+    # Create new recipe to add (( EDIT THESE ))
+    name = "Sugar Cookie"
+    ingredients = ["butter", "sugar", "flour"]
+    link = "https://bellyfull.net/easy-sugar-cookie-recipe/"
 
-# Append new recipe to recipes list
-recipes.append(new_recipe)
+    # Create dictionary for new recipe
+    new_recipe = {"Name": name.title(),
+                  "Ingredients": [ingredient.lower() for ingredient in ingredients],
+                  "Link": link}
 
-with open("recipes.json", "w") as file:
-    # Update recipes.json
-    json.dump(recipes, file)
+    # Make sure the name isn't already in recipes
+    for r in recipes:
+        if r["Name"] == name:
+            print(f"{name} already in recipes")
+            return
+
+    # Append new recipe to recipes list
+    recipes.append(new_recipe)
+
+    with open("recipes.json", "w") as file:
+        # Update recipes.json
+        json.dump(recipes, file)
+
+    print(f"Added {name} to recipes")
+
+
+if __name__ == "__main__":
+    main()
--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    x = 2
     return render_template("index.html")
 
 
