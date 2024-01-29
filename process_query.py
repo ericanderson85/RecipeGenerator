ac04807633edb6f4117e74f182d267080bc73545
Yoken0 <kenzomatsuo@gmail.com>
2024-01-29 18:30:18 -0500

    added an ingredient element searcher that returns recipes with given elements

--- process_query.py
+++ process_query.py
@@ -2,7 +2,15 @@ import json
 # gets recipes data from json file
 with open("recipes.json", "r") as file:
     file = json.load(file)
+#TODO user input is set for now, will work on getting from website
+userInput  = ["butter"]
 
-userInput  = "eggs"
+# recipe list that contains the ingredients given
+recipesList = []
 
+for i in file:
+    test = i["Ingredients"]
+    if all(element in test for element in userInput):
+        recipesList.append(i["Name"])
 
+print(recipesList)
