d92168e17a18efc546d17aa0a639e91e77d2aa80
Fernando Hernandez <fernandohernandez@fernandos-air.ssw.umb.edu>
2024-01-31 16:02:03 -0500

    Fixed Typo

--- remove_recipes.py
+++ remove_recipes.py
@@ -4,23 +4,23 @@ import json
 Name = input('Enter Name of Recipe to Delete\n')
 
 # Current recipeslist
-recipies = []
+recipes = []
 
 # Updated recipes list
-new_recipies = []
+new_recipes = []
 
 # Opens recipies list and assigns it to current recipies
 with open("static/recipes.json", "r") as file:
-    recipies = json.load(file)
+    recipes = json.load(file)
 
 # Appends recipies to updated recipies unless it is the recipe to remove 
-for r in recipies:
+for r in recipes:
         if r["Name"] == Name:
                print("Recipe Deleted")
         else:
-               new_recipies.append(r)
+               new_recipes.append(r)
              
 # Updates the new recipe list to recipes              
 with open("static/recipes.json", "w") as file:
         # Update recipes.json
-        json.dump(new_recipies, file, indent=4)
+        json.dump(new_recipes, file, indent=4)
