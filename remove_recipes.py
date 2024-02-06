d37d060ffe6e651a11d64b1fb5df9a16da924760
ericanderson85 <ericandersonnn@icloud.com>
2024-02-05 21:55:18 -0500

    reformatting

--- remove_recipes.py
+++ remove_recipes.py
@@ -19,34 +19,32 @@ ingredients = []
 with open("static/recipes.json", "r") as file:
     recipes = json.load(file)
 
-# Appends recipies to updated recipies unless it is the recipe to remove 
+# Appends recipies to updated recipies unless it is the recipe to remove
 for r in recipes:
-        if r["Name"] == Name:
-               ingredient_removed.append(r["Ingredients"])
-               print("Recipe Deleted")
-        else:
-               ingredients.append(r["Ingredients"])
-               new_recipes.append(r)
-             
-# Updates the new recipe list to recipes              
+    if r["Name"] == Name:
+        ingredient_removed.append(r["Ingredients"])
+        print("Recipe Deleted")
+    else:
+        ingredients.append(r["Ingredients"])
+        new_recipes.append(r)
+
+# Updates the new recipe list to recipes
 with open("static/recipes.json", "w") as file:
-        # Update recipes.json
-        json.dump(new_recipes, file, indent=4)
+    # Update recipes.json
+    json.dump(new_recipes, file, indent=4)
 
-# Iterates through lists and leaves behind unique ingredients to be removed 
+# Iterates through lists and leaves behind unique ingredients to be removed
 for item in ingredients:
-       for items in item:
-              for rm in ingredient_removed:
-                     for rms in rm:
-                            if items == rms:
-                                          ingredient_removed[0].remove(items)
+    for items in item:
+        for rm in ingredient_removed:
+            for rms in rm:
+                if items == rms:
+                    ingredient_removed[0].remove(items)
 
-# Calls to the ingredient remover and removes 
+# Calls to the ingredient remover and removes
 # all unique ingredients from removed recipe
 for r in ingredient_removed:
-       for t in r:            
-              child = subprocess.Popen(["python", "remove_ingredients.py"], stdin=subprocess.PIPE)
-              child.communicate(input=t.encode())
-
-
-       
+    for t in r:
+        child = subprocess.Popen(
+            ["python", "remove_ingredients.py"], stdin=subprocess.PIPE)
+        child.communicate(input=t.encode())
