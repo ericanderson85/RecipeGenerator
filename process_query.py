352392da3252e622d7d90a6159cef2aebd722e43
ericanderson85 <ericandersonnn@icloud.com>
2024-02-05 21:52:42 -0500

    combine functions

--- process_query.py
+++ process_query.py
@@ -7,27 +7,11 @@ with open("static/recipes.json", "r") as all_recipes:
 
 # Returns all recipes that can be made and all recipes that can almost be made
 def recipes(ingredients):
-    print(ingredients)
-    possible = possible_recipes(ingredients)
-    print(possible)
-    almost = [element for element in recipe_search(
-        ingredients) if element not in possible]
-    return (possible, almost)
-
-
-# Returns all recipes that can be made with the given ingredients
-def possible_recipes(ingredients):
     possible_recipes_list = []
+    recipe_search_list = []
     for recipe in all_recipes:
         if all(ingredient in ingredients for ingredient in recipe["Ingredients"]):
             possible_recipes_list.append(recipe)
-    return possible_recipes_list
-
-
-# Returns all recipes that can almost be made with the given ingredients
-def recipe_search(ingredients):
-    recipe_search_list = []
-    for recipe in all_recipes:
-        if any(ingredient in ingredients for ingredient in recipe["Ingredients"]):
+        elif any(ingredient in ingredients for ingredient in recipe["Ingredients"]):
             recipe_search_list.append(recipe)
-    return recipe_search_list
+    return possible_recipes_list, recipe_search_list
